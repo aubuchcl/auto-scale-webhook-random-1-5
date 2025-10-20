33feab53dd541787e8607454cc0644a90fa3e9ab
Chris Aubuchon <aubuchcl@gmail.com>
2025-10-20 10:51:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON and text bodies
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+// Existing random endpoint
 app.get('/random', (req, res) => {
   const number = Math.floor(Math.random() * 5) + 1;
   res.json({ instances: number });
+});
+
+// New /print endpoint
+app.post('/print', (req, res) => {
+  // Return whatever was sent in the request body
+  res.json({ received: req.body });
 });
 
 app.listen(port, () => {
