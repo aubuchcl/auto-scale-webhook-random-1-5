795d39cb8860482414e0c8375092efd0173aa5ff
Chris Aubuchon <aubuchcl@gmail.com>
2025-10-21 14:11:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,17 @@ app.post('/print', (req, res) => {
   res.json({ instances: number });
 });
 
+app.get('/print', (req, res) => {
+  // Log the request body in formatted JSON
+  console.log(JSON.stringify(req.body, null, 2));
+
+  // Generate a random number between 1 and 5
+  const number = Math.floor(Math.random() * 5) + 1;
+
+  // Send response
+  res.json({ instances: number });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
